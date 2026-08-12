54d1c42bc3702051cd3343854225674ec97152db
brianhzh <brianzninja@gmail.com>
2026-08-12 16:49:05 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/portfolio-website/',
   plugins: [react()],
   server: {
     // /mnt/c (NTFS through WSL) doesn't emit file events, so hot reload needs polling
